834a5fd08e362d056004c85a0141ad8f5a05ed5d
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-08-17 22:51:44 +0530

    ✨ Feature coloured bar (#3)
    * 🔧 New Progress bar
    
    * 🚑 Fix
    
    * 🚑 Fix 2
    
    * 🚑 Final Fix
    
    * Update GenerateREADME.py
    
    * 🚑 Fix
    
    * Delete test

--- utility.py
+++ utility.py
@@ -15,10 +15,9 @@ def makeLanguageList(data: list) -> str:
     for l in data:
         ln = len(l['name'])
         ln_text = len(l['text'])
-        op = f"""
-|{l['name'][:25]}{' ' * (25 - ln)}|{l['text']}|{' ' * (20 - ln_text)}{makeGraph(l['percent'])}|{l['percent']}%|\n"""
+        op = f"""|{l['name'][:25]}{' ' * (25 - ln)}|{l['text']}|{' ' * (20 - ln_text)}{makeGraph(l['percent'])}|{l['percent']}%|"""
         data_list.append(op)
-    return ' '.join(data_list)
+    return '\n'.join(data_list)
 
 
 def makeCommitList(data: list) -> str:
@@ -27,6 +26,7 @@ def makeCommitList(data: list) -> str:
     for l in data[:7]:
         ln = len(l['name'])
         ln_text = len(l['text'])
-        op = f"""|{l['name']}{' ' * (13 - ln)}|{l['text']}{' ' * (15 - ln_text)}|{makeGraph(l['percent'])}|{l['percent']}%|\n"""
+        op = f"""|{l['name']}{' ' * (13 - ln)}|{l['text']}{' ' * (15 - ln_text)}|{makeGraph(l['percent'])}|{l['percent']}%|"""
         data_list.append(op)
-    return ' '.join(data_list)
+    return '\n'.join(data_list)
+    
